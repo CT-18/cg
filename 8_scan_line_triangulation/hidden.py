36907445b38ffa9f6157223548969d36bdecd21e
sandwwraith <sandwwraith@gmail.com>
2017-01-13 04:59:16 +0300

    Draft copy (working code, no theory)

---
+++ 8_scan_line_triangulation/hidden.py
@@ -0,0 +1,92 @@
+import matplotlib.pyplot as plt
+
+def turn(v1, v2, v3):
+    return (v2.x - v1.x) * (v3.y - v1.y) - (v3.x - v1.x) * (v2.y - v1.y)
+
+class Vertex:
+    """
+    Класс точки с компаратором, подходящим для нашей задачи
+    """
+    def __init__(self, px, py):
+        self.x = px
+        self.y = py
+        self.hedgelist = []
+        self.vtype = None
+    
+    def __lt__(self, other):
+        d = self.y - other.y
+        if d == 0: 
+            d = other.x - self.x
+        return d > 0
+    
+    def __eq__(self, other):
+        return self.x == other.x and self.y == other.y
+
+    def __repr__(self):
+        return '({0};{1})'.format(self.x,self.y)
+
+    
+class Hedge:
+    def __init__(self, v):
+        self.origin = v
+        self.twin = None
+        self.next = None
+        self.prev = None
+        self.helper = None
+        
+    def __lt__(self, other):
+        """
+        Сравнение для дерева поиска рёбер в сканлайне 
+        """
+        v1 = self.origin
+        v2 = self.twin.origin
+        v3 = other.origin
+        t = turn(v1, v2, v3)
+        return t > 0
+        
+    def __repr__(self):
+        return '{}->{}'.format(self.origin, self.twin.origin)
+    
+
+def build_dcel(vert):
+    dcel = []
+    start = Hedge(vert[0])
+    start.twin = Hedge(vert[1])
+    vert[1].hedgelist.append(start.twin)
+    start.twin.twin = start
+    dcel.append(start)
+    prev = start
+    lis = vert[1:] + [vert[0]]
+    for i in range(0, len(lis)-1):
+        cur = Hedge(lis[i])
+        lis[i].hedgelist.append(cur)
+        cur.twin = Hedge(lis[i+1])
+        lis[i+1].hedgelist.append(cur.twin)
+        cur.twin.twin = cur
+        cur.prev = prev
+        prev.next = cur
+        prev.twin.prev = cur.twin
+        cur.twin.next = prev.twin
+        prev = cur
+        dcel.append(cur)
+    cur.next = start
+    start.prev = cur
+    start.twin.next = cur.twin
+    cur.twin.prev = start.twin
+    vert[0].hedgelist.append(start)
+    return dcel
+
+def add_diagonal(hfrom, hto):
+    print ('[ADD DIAGONAL] from: {} to: {}'.format(hfrom.origin, hto.origin))
+    d = Hedge(hfrom.origin)
+    d.twin = Hedge(hto.origin)
+    hfrom.prev.next = d
+    d.prev = hfrom.prev
+    hfrom.prev = d.twin
+    d.twin.next = hfrom
+    hto.prev.next = d.twin
+    d.twin.prev = hto.prev
+    hto.prev = d
+    d.next = hto
+    return d
+
---
+++ 8_scan_line_triangulation/visual_utils.py
@@ -0,0 +1,109 @@
+import re
+import os
+
+import matplotlib.pyplot as plt
+import ipywidgets as widgets
+import traitlets
+
+def natural_sort(l): 
+    convert = lambda text: int(text) if text.isdigit() else text.lower() 
+    alphanum_key = lambda key: [ convert(c) for c in re.split('([0-9]+)', key) ] 
+    return sorted(l, key = alphanum_key)
+
+def draw_points(source, types=True):
+    def type_color(v):
+        if v.vtype == 'regular':
+            return 'ok'
+        if v.vtype == 'start':
+            return 'sy'
+        if v.vtype == 'end':
+            return 'sg'
+        if v.vtype == 'split':
+            return '^r'
+        if v.vtype == 'merge':
+            return 'vb'
+    xmin, xmax = min([i.x for i in source]) - 2, max([i.x for i in source]) + 2
+    ymin, ymax = min([i.y for i in source]) - 2, max([i.y for i in source]) + 2
+    plt.plot([i.x for i in source + [source[0]]], [i.y for i in source + [source[0]]],'k-', zorder=0)
+    if types:
+        for v in source:
+            plt.plot(v.x, v.y,type_color(v), zorder=1)
+    plt.axis([xmin,xmax, ymin,ymax])
+    plt.show()
+
+def visual_dump_state(cur_v, D,D1,T,Q,filename):
+    xmin, xmax = min([i.x for i in Q]) - 2, max([i.x for i in Q]) + 2
+    ymin, ymax = min([i.y for i in Q]) - 2, max([i.y for i in Q]) + 2
+    fig = plt.figure()
+    for h in D:
+        xs = [h.origin.x, h.twin.origin.x]
+        ys = [h.origin.y, h.twin.origin.y]
+        plt.plot(xs,ys,'k-')
+    for h in D1:
+        xs = [h.origin.x, h.twin.origin.x]
+        ys = [h.origin.y, h.twin.origin.y]
+        plt.plot(xs,ys,'b:')
+    for h in T:
+        xs = [h.origin.x, h.twin.origin.x]
+        ys = [h.origin.y, h.twin.origin.y]
+        plt.plot(xs,ys,'b-')
+    for q in Q:
+        if cur_v is None or q < cur_v:
+            plt.plot(q.x, q.y, 'ko')
+    if not cur_v is None:
+        plt.plot(cur_v.x, cur_v.y, 'ro')
+    plt.axis([xmin,xmax, ymin,ymax])
+    plt.savefig(filename)
+    plt.close(fig)
+
+def visual_dump_triang(cur_v,D,D1,S,Q,filename):
+    xmin, xmax = min([i.x for i in Q]) - 2, max([i.x for i in Q]) + 2
+    ymin, ymax = min([i.y for i in Q]) - 2, max([i.y for i in Q]) + 2
+    fig = plt.figure()
+    for h in D:
+        xs = [h.origin.x, h.twin.origin.x]
+        ys = [h.origin.y, h.twin.origin.y]
+        plt.plot(xs,ys,'k-')
+    for h in D1:
+        xs = [h.origin.x, h.twin.origin.x]
+        ys = [h.origin.y, h.twin.origin.y]
+        plt.plot(xs,ys,'b:')
+    for q in Q:
+        if cur_v is None or q < cur_v:
+            plt.plot(q.x, q.y, 'ko')
+    for q in S:
+        plt.plot(q.x, q.y, 'yo')
+    if not cur_v is None:
+        plt.plot(cur_v.x, cur_v.y, 'ro')
+    plt.axis([xmin,xmax, ymin,ymax])
+    plt.savefig(filename)
+    plt.close(fig)
+    
+def SlideShower(folder, frame_duration=1000):
+    slides = list([open(os.path.join(folder,s), 'rb').read()
+                   for s in natural_sort(os.listdir(folder))])
+
+    img = widgets.Image(value=slides[0],width=600,height=400)
+
+    def on_frame(change):
+        n = change['new']
+        img.value = slides[n]
+
+    play = widgets.Play(
+        value=0,
+        min=0,
+        max=len(slides)-1,
+        step=1,
+        interval=frame_duration,
+        disabled=False
+    )
+    slider = widgets.IntSlider(
+        value=0,
+        min=0,
+        max=len(slides)-1,
+        step=1
+    )
+    slider.observe(on_frame, names='value')
+    widgets.jslink((play, 'value'), (slider, 'value'))
+    box = widgets.VBox([img, widgets.HBox([play, slider])])
+    return box
