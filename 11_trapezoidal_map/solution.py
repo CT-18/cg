fb3cd6f0ed93c96cc247b16b3874ec1f7b888010
kohanov <alexkoh192spb@mail.ru>
2017-01-19 22:45:15 +0300

    vyorstka, code and pictures improved; requirements added

--- 11_trapezoidal_map/solution.py
+++ 11_trapezoidal_map/solution.py
@@ -3,57 +3,52 @@ from numpy import *
 
 """Структура, описывающая отрезок"""
 class Segment():
-    p = [0.0, 0.0]
-    "Правая точка"
-    q = [1.0, 1.0]
-    
     "Предполагаем, что p и q упорядочены лексикографически"
     def __init__(self, p, q):
+        "Левая точка"
         self.p = p
+        "Правая точка"
         self.q = q
 
 """Структура, описывающая трапецоид"""
 class Trapezoid():
-    "Верхний и нижний отрезки"
-    top = None
-    bottom = None
-
-    "Левая и правая точки"
-    leftp = None
-    rightp = None
-    
-    "Соседи трапецоида"
-    leftnb = [None, None]
-    rightnb = [None, None]
-    
-    "Ссылки на узлы локализационной структуры"
-    links = [None]
     
     def __init__(self, top, bottom, leftp, rightp):
+        "Верхний и нижний отрезки"
         self.top = top
         self.bottom = bottom
+        
+        "Левая и правая точки"
         self.leftp = leftp
         self.rightp = rightp
+        
+        "Соседи трапецоида"
+        self.leftnb = [None, None]
+        self.rightnb = [None, None]
+        
+        "Ссылки на узлы локализационной структуры"
         self.links = []
 
 class Node():
-        "-1 для точки p; 0 для отрезка; 1 для точки q"
-        mytype = 0
-        segment = None
-        left = [None, None] # нужно знать, куда ведет ребро: на узел или на трапецоид 
-        right = [None, None] # False - трапецоид, True - узел
+        
         def __init__(self, nodeType, s):
+            "Тип узла: -1 для точки p, 0 для отрезка s, 1 для точки q"
             self.mytype = nodeType
+            "Сылка на отрезок, по которому определяется направление обхода графа"
             self.segment = s
+            "Левое и правое ребро"
+            "Если оно ведет на другой узел, то в первой ячейке True"
+            "Если на трапецоид, то False"
+            "Во второй ячейке ссылка на сам узел/трапецоид"
+            self.left = [None, None]
+            self.right = [None, None]
 
 class TrapezoidalMap():
-    "Список всех трапецоидов, для отрисовки"
-    tr = []
-    "Корень поисковой структуры"
-    root = None
-
+    
     def __init__(self):
+        "Список всех трапецоидов, для отрисовки"
         self.tr = [Trapezoid(None, None, None, None)]
+        "Корень поисковой структуры"
         self.root = None
     
     "Локализация точки на карте"
@@ -136,3 +131,46 @@ class TrapezoidalMap():
         trapezoid = self.localize(s.p)
         "TODO: дописать вставку"
         self.__insertInside(s, trapezoid)
+
+"Методы для отрисовки трапецоида"
+def perp(a):
+    b = empty_like(a)
+    b[0] = -a[1]
+    b[1] = a[0]
+    return b
+
+def intersectionPoint(a1, a2, b1, b2):
+    a1 = array([a1[0], a1[1]])
+    a2 = array([a2[0], a2[1]])
+    b1 = array([b1[0], b1[1]])
+    b2 = array([b2[0], b2[1]])
+    da = a2-a1
+    db = b2-b1
+    dp = a1-b1
+    dap = perp(da)
+    denom = dot( dap, db)
+    num = dot( dap, dp )
+    return (num / denom.astype(float))*db + b1
+
+def points(trapezoid):
+    q1 = [0, 0]
+    q2 = [0, 5]
+    q3 = [5, 5]
+    q4 = [5, 0]
+    if trapezoid.leftp != None:
+        q1[0] = trapezoid.leftp[0]
+        q2[0] = trapezoid.leftp[0]
+    if trapezoid.rightp != None:
+        q3[0] = trapezoid.rightp[0]
+        q4[0] = trapezoid.rightp[0]
+    if trapezoid.top != None:
+        intp = intersectionPoint(q1, q2, trapezoid.top.p, trapezoid.top.q)
+        q2[1] = intp[1]
+        intp = intersectionPoint(q3, q4, trapezoid.top.p, trapezoid.top.q)
+        q3[1] = intp[1]
+    if trapezoid.bottom != None:
+        intp = intersectionPoint(q1, q2, trapezoid.bottom.p, trapezoid.bottom.q)
+        q1[1] = intp[1]
+        intp = intersectionPoint(q3, q4, trapezoid.bottom.p, trapezoid.bottom.q)
+        q4[1] = intp[1]
+    return [[q1[0], q2[0], q3[0], q4[0], q1[0]], [q1[1], q2[1], q3[1], q4[1], q1[1]]]
