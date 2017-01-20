3c87d746b4f69be2398b5771bcbe92c7e67c39d8
qradimir <rad.sorokin@gmail.com>
2017-01-20 22:00:14 +0300

    skip quardtree: add cqtree interactive sandbox

--- 1_skip_quardtree/utils/view.py
+++ 1_skip_quardtree/utils/view.py
@@ -36,9 +36,31 @@ def apply_operation(tree, op):
         raise Exception('Uhdefined command')
 
 def display_cqtree_dump(tree, ops, scale=3):
-    plot, axes = plt.subplots(1, len(ops), figsize=(scale * len(ops), scale))
+    fig, axes = plt.subplots(1, len(ops), figsize=(scale * len(ops), scale))
     for op, axis in zip(ops, axes):
         apply_operation(tree, op)
         debug_cqtree(tree, axis)
         axis.set_title("{} ({}, {})".format(*op))
-    return plot
+
+viewer = None
+        
+class TreeViewer:
+    
+    def __init__(self, tree, ax):
+        self.tree = tree
+        self.ax = ax
+        ax.figure.canvas.mpl_connect('button_press_event', self.press_callback)
+        
+    def press_callback(self, event):
+        self.tree.insert((event.xdata, event.ydata))
+        self.ax.clear()
+        debug_cqtree(self.tree, self.ax)
+        display(self.ax.figure)
+
+
+def display_cqtree_interactive(tree, scale=3):
+    fig = plt.figure(1, figsize=(scale, scale))
+    ax = plt.subplot(111)
+    global viewer
+    viewer = TreeViewer(tree, ax)
+    debug_cqtree(tree, ax)
