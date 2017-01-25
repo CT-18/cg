b86e2c3fa312f683fbb922f6047831437e4499d3
BioRyajenka <igor-sushencev@yandex.ru>
2017-01-25 19:41:16 +0300

    fin

--- 18_localization_delaunay/dcel.py
+++ 18_localization_delaunay/dcel.py
@@ -1,26 +1,17 @@
-class vertex(object):
+class vertex:
     def __init__(self, name, x, y):
-        self.x = x
-        self.y = y
+        self.x, self.y, self.z = x, y, 1
+        self.triangles = []
+        self.name = name
+        
+    def add_triangle(self, triangle):
+        self.triangles.append(triangle) # список треугольников, инцидентных вершине, в порядке обхода по часовой стрелке
+
+class triangle:
+    def __init__(self, name, vertices):
+        self.vertices = vertices # лист вершин в порядке обхода по часовой стрелке
+        self.neighbours = []
         self.name = name
     
-    def set_edge(self, edge):
-        self.edge = edge
-
-class face(object):
-    def __init__(self, name, outer_edge, inner_edges):
-        self.outer_edge = outer_edge
-        self.inner_edges = inner_edges
-        self.name = name
-
-class hedge(object):
-    def __init__(self, origin_vertex):
-        self.origin_vertex = origin_vertex
-
-    def set_edges(self, prev_edge, next_edge, twin_edge):
-        self.prev_edge = prev_edge
-        self.next_edge = next_edge
-        self.twin_edge = twin_edge
-
-    def set_face(self, incident_face):
-        self.incident_face = incident_face
+    def add_neighbour(self, neighbour):
+        self.neighbours.append(neighbour) # лист смежных по ребру треугольников-соседей, в порядке обхода по часовой стрелке
