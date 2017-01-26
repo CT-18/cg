849acaf55f0cf59833d257eedf06b5848adebc55
kohanov <alexkoh192spb@mail.ru>
2017-01-26 12:07:20 +0300

    pictures and slideshow updated

--- 11_trapezoidal_map/examples.py
+++ 11_trapezoidal_map/examples.py
@@ -1,44 +1,37 @@
 import ipywidgets as widgets
 from IPython.display import Image
 
-widget = None
-def insertion_slideshow():
-    def browse_images(arr):
-        n = len(arr)
-        def view_image(i):
-            global widget
-            f = open(arr[i], "rb")
-            image = f.read()
-            if widget != None:
-                widget.value = image
-                return
-            widget = widgets.Image(value=image, format='jgp', width='80%')
-            return widget
-        widgets.interact(view_image, i=(0, n-1))
-    
+def slideshow(folder = 'insert', period = 1500):
     images = []
-    for i in range(8):
-        imageName = 'images/segment/'+ str(i + 1) + '.jpg'
-        images.append(imageName)
-    browse_images(images)
+    if folder == 'insert':
+        n = 8
+        image_width = '80%'
+    else:
+        n = 12
+        image_width = '100%'
+    for i in range(n):
+        imageName = 'images/' + folder + '/'+ str(i + 1) + '.jpg'
+        images.append(open(imageName, 'rb').read())
+    widget = widgets.Image(value=images[0], format='jgp', width=image_width)
     
-lwidget = None
-def localization_slideshow():
-    def browse_images(arr):
-        n = len(arr)
-        def view_image(i):
-            global lwidget
-            f = open(arr[i], "rb")
-            image = f.read()
-            if lwidget != None:
-                lwidget.value = image
-                return
-            lwidget = widgets.Image(value=image, format='jgp', width='100%')
-            return lwidget
-        widgets.interact(view_image, i=(0, n-1))
+    def view_image(i):
+        widget.value = images[i['new']]
     
-    images = []
-    for i in range(12):
-        imageName = 'images/map/'+ str(i + 1) + '.jpg'
-        images.append(imageName)
-    browse_images(images)
+    play = widgets.Play(
+        value=0,
+        min=0,
+        max=n,
+        step=1,
+        interval=period,
+        disabled=False
+    )
+    slider = widgets.IntSlider(
+        value=0,
+        min=0,
+        max=n-1,
+        step=1
+    )
+    slider.observe(view_image, names='value')
+    widgets.jslink((play, 'value'), (slider, 'value'))
+    box = widgets.VBox([widget, widgets.HBox([play, slider])])
+    return box
