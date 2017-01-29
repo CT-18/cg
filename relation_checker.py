572b062b7576a7d19631246e929a0d162cb8d24a
Lebedev Roman <lebedeffroman@gmail.com>
2017-01-29 04:13:35 +0300

    add ex2 for segment tree

---
+++ relation_checker.py
@@ -0,0 +1,36 @@
+import matplotlib.pyplot as plt
+
+def solve(s, i):
+    if i[0] < s[0] and s[1] <= i[1]:
+        return 'contains'
+    if s[0] <= i[1] and i[0] < s[1]:
+        return 'intersects'
+    return 'does not intersect'
+
+def show_ex(s, i):
+    plt.axis([0, 9, 0, 4])
+    plt.plot(s, [1,1], c='r')
+    plt.scatter(s, [1,1], c='r')
+    plt.plot(i, [2,2], c='b')
+    plt.scatter(i, [2,2], c='b')
+    plt.legend(['segment','interval'])
+    plt.show()
+
+def show_error(s, i, res):
+    print('Неправильный ответ!!!')
+    print('Ваш ответ: ', res)
+    print('Не обращайте внимания на ось Оу. Она приведена только для наглядности.')
+    show_ex(s, i)
+    
+def test(relation):
+    i = [3, 6]
+    for a in range(1, 8):
+        for b in range(a + 1, 9):
+            s = [a, b]
+            result = relation(s, i)
+            expected = solve(s, i)
+            if result != expected:
+                show_error(s, i, result)
+                return
+
+    print('Вы справились! Функция реализвана правильно!')
