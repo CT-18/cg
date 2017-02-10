a2f8621895f49ca7c4ce9eaff758774a3f106062
rthakohov <ruslan.thakokhov@gmail.com>
2017-02-10 18:22:40 +0300

    Code for the first exercise written.

--- binary_search_tree.py
+++ binary_search_tree.py
@@ -1,3 +1,43 @@
+class TreeNode:
+    def __init__(self, value):
+        self.left = None
+        self.right = None
+
+        self.value = value
+
+class InnerTree:
+    def __init__(self, value, binaryTree):
+        self.value = value
+        self.binaryTree = binaryTree
+
 class BinarySearchTree:
-	def __init__(self):
-		self.root = 0
+    def __init__(self):
+        self.root = None
+        self.size = 0
+
+    def insert(self, value):
+        node = self.root
+        newNode = TreeNode(value)
+
+        if node:
+            prevNode = None
+
+            while node:
+                prevNode = node;
+                if cmpValues(newNode.value, node.value):
+                    node = node.left
+                else:
+                    node = node.right
+
+            if cmpValues(newNode.value, prevNode.value):
+                prevNode.left = newNode
+            else:
+                prevNode.right = newNode
+        else:
+            self.root = newNode
+
+def cmpValues(lhs, rhs):
+    if type(lhs) == InnerTree:
+        return lhs.value < rhs.value
+    else :
+        return lhs < rhs
--- test_tree_build.py
+++ test_tree_build.py
@@ -1,6 +1,48 @@
-from ipywidgets import Button, Layout
+import binary_search_tree
 
-def test():
-	b = Button(description='Проверить решение',
-	           layout=Layout(width='50%', height='80px'))
-	b
+from random import randint
+
+TEST_SIZE = 100
+
+def testBuildTree(testFunction):
+    test = generateTest()
+
+    result = testFunction(test)
+    expectedResult = buildTree(test)
+
+def generateTest():
+    points = []
+
+    for i in range(0, TEST_SIZE):
+        points.append((randint(-10, 10), randint(-10, 10)))
+
+    return points
+
+def buildTree(points):
+    tree = binary_search_tree.BinarySearchTree()
+
+    orderedPoints = []
+    xValues = []
+
+    for x, y in points:
+        if len(orderedPoints[x]) == 0:
+            orderedPoints[x] = []
+            xValues.append(x)
+
+        orderedPoints[x].append(y)
+
+    for xValue in xValues:
+        tree.insert(binary_search_tree.InnerTree(xValue, orderedPoints[xValue]))
+
+    return tree
+
+def buildInnerTree(values):
+    tree = binary_search_tree.BinarySearchTree()
+
+    for val in values:
+        tree.insert(val)
+
+    return tree
+
+def compareTrees(lhs, rhs):
+    return 1
