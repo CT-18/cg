a53b1f4ad38f398b7dca84859f1170dc19566f75
rthakohov <ruslan.thakokhov@gmail.com>
2017-02-10 18:19:18 +0300

    First exercise added

---
+++ binary_search_tree.py
@@ -0,0 +1,3 @@
+class BinarySearchTree:
+	def __init__(self):
+		self.root = 0
---
+++ test_tree_build.py
@@ -0,0 +1,6 @@
+from ipywidgets import Button, Layout
+
+def test():
+	b = Button(description='Проверить решение',
+	           layout=Layout(width='50%', height='80px'))
+	b
