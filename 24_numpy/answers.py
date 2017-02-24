c7a31704196956b8f6eb850ee74dc10dccbf4708
Eugene Nemchenko <n.eugene536@gmail.com>
2017-02-24 23:48:55 +0300

    looks good

---
+++ 24_numpy/answers.py
@@ -0,0 +1,50 @@
+import numpy as np
+from numpy import pi
+
+# returns max angle of log(|sin * cos|) 
+def task1_ans():
+    angles = np.arange(10, 360, 10)
+    radians = angles * pi / 180
+    fs  = np.log(np.abs(np.cos(radians) * np.sin(radians)))
+
+    return angles[fs.argmax()]
+
+def task2_ans(img, t):
+    new_img = img.copy()
+    near_zero = new_img < t
+    new_img[near_zero] = 0
+    new_img[~near_zero] = 255
+
+    return new_img
+
+def test_task2_impl(img, t, task2):
+    o_img = img.copy()
+    expexted_res = task2_ans(img, t)
+    given_res = task2(img, t)
+
+    if np.array_equal(expexted_res, given_res):
+        if np.array_equal(img, o_img):
+            return True
+        else:
+            print("Oh no you corrupt source image !!! =(")
+
+    else:
+        print("img:", img)
+        print("t:", t)
+        print("expected: ", expexted_res)
+        print("given: ", given_res)
+
+    return False
+
+def test_task2(task2):
+    img = np.array([[1, 2],
+                    [3, 4]])
+    t = 3
+
+    img2 = np.arange(0, 1000)
+    img2.reshape((20, 50))
+    t2 = 340
+
+    return test_task2_impl(img, t, task2) and test_task2_impl(img2, t2, task2)
+
+
