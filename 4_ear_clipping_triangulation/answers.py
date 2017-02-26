84b23763498c47f09d3110a5617588c0f01e298d
Lebedev Roman <lebedeffroman@gmail.com>
2017-02-26 15:39:32 +0300

    add is_outer checker fix (assumption about vertexes overlay)

--- 4_ear_clipping_triangulation/answers.py
+++ 4_ear_clipping_triangulation/answers.py
@@ -1,5 +1,8 @@
 from hidden import turn
 
 def is_outer_answer(v0,v1,v2,v):
-    return turn(v0, v1, v)<= -1 or turn(v1, v2, v) <= -1 or turn(v2, v0, v) <= -1 
+    # Допущение для триангуляции многоугольников с дырками, позволяющее сохранять полигон
+    # корректным после слияния внешней и внутренней частей двумя налегающими друг на друга ребрами.
+    prop = v == v0 or v == v1 or v == v2
+    return prop or turn(v0, v1, v)<= -1 or turn(v1, v2, v) <= -1 or turn(v2, v0, v) <= -1 
 
