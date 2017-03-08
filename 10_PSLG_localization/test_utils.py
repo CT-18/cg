e7289717d90fb06d651eefa42b0fecd5841d4d93
Dmitry Novik <d1mas1k4@yandex.ru>
2017-03-08 17:18:41 +0300

    New task introduced. Formalization of important definitions is added. New notes about time complexity of slabs method.

--- 10_PSLG_localization/test_utils.py
+++ 10_PSLG_localization/test_utils.py
@@ -54,13 +54,49 @@ class Slab:
 
         if point is not None:
             plt.plot(point[0], point[1], 'o')
-	    
+        
         plt.margins(0.1)
         plt.show()
 
 
 def convert_edge(edge):
-	x1, y1 = edge[0]
-	x2, y2 = edge[1]
-	return ([x1, x2], [y1, y2])
+    x1, y1 = edge[0]
+    x2, y2 = edge[1]
+    return ([x1, x2], [y1, y2])
 
+
+def correct_comparator(a, b):
+    if turn(a[0], a[1], b[0]) * turn(a[0], a[1], b[1]) == 1:
+        if turn(a[0], a[1], b[0]) < 0:
+            return True
+        else:
+            return False
+    else:
+        return not correct_comparator(b, a)
+
+
+
+def test_comparator(comp):
+    edges = [
+    [(0, 0), (1, 0)],
+    [(0, 1), (1, 1)],
+    [(0, 0), (1, 0)],
+    [(0, 0), (1, 1)],
+    [(0, 0), (1, 0)],
+    [(0, 1), (1, 0)],
+    [(0, 0), (1, 0)],
+    [(-4, 1), (8, 9)],
+    [(-5, -1), (1, 9)],
+    [(0, 0), (1, 0)],
+    [(0, 0), (1, 0)],
+    [(0, 9), (4, -1)]
+    ]
+    
+    for i in range(0, len(edges), 2):
+        print("Running test #", (i // 2) + 1)
+        if comp(edges[i], edges[i + 1]) != correct_comparator(edges[i], edges[i + 1]):
+            print("Failure")
+            return
+        print("Passed")
+        
+    print("Passed all tests")
