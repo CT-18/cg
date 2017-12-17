fee10e07d41cc89a632284db30c02260608cb4b0
Oktai <oktai.tatanov@gmail.com>
2017-12-17 22:02:31 +0300

    Minor: fix bug ray

--- 23_triangulation_in_mem/my_utils.py
+++ 23_triangulation_in_mem/my_utils.py
@@ -10,15 +10,30 @@ def is_segment(maybe_s):
     return isinstance(maybe_s, Segment) 
 
 
-def inf_ray(a, b, k):
-    ray = None
-    step_x = abs(a.x - b.x)
-    step_y = abs(a.y - b.y)
-    new_b = Point(b.x + step_x, b.y + step_y) 
+def inf_ray(a, b, mult):
+    ab = Line(a, b)
+    c_x, c_y, c_b = ab.coefficients
+    step_x = a.x - b.x if a.x > b.x else b.x - a.x 
+    step_y = a.y - b.y if a.y > b.y else b.y - a.y
+    new_b = None
+    k = 0
     
-    if (Ray(a, b).contains(new_b)):
-        b = Point(b.x + k * step_x, b.y + k * step_y)
-    else:
-        b = Point(b.x - k * step_x, b.y - k * step_y)
+    if c_y != 0:
+        k = -(c_x / c_y)
         
+    if k < 0:
+        new_b = Point(b.x + step_x, b.y - step_y)
+        
+        if (Ray(a, b).contains(new_b) and not new_b.equals(a)):
+            b = Point(b.x + mult * step_x, b.y - mult * step_y)
+        else:
+            b = Point(b.x - mult * step_x, b.y + mult * step_y)
+    else:
+        new_b = Point(b.x + step_x, b.y + step_y)
+        
+        if (Ray(a, b).contains(new_b) and not new_b.equals(a)):
+            b = Point(b.x + mult * step_x, b.y + mult * step_y)
+        else:
+            b = Point(b.x - mult * step_x, b.y - mult * step_y)
+            
     return (b, Ray(a, b))
